8e4463f41de8c36d06adc12c1a76cb82e72228b8
mazavr <boxblast@gmail.com>
2013-08-01 15:56:30 +0400

    grid story actions

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -16,22 +16,30 @@ module StoriesHelper
           concat content_tag(:span, '', class: 'caret')
         }
         concat(content_tag(:ul, class: 'dropdown-menu') do
-          '<li></li>'
+          concat content_tag(:li) {
+            concat link_to 'Accept', story_path(story, 'story[state]' => 'accepted'), method: :put
+          }
+          if story.user
+            concat content_tag(:li) {
+              concat link_to 'Start', story_path(story, 'story[state]' => 'started'), method: :put
+            }
+          else
+            concat content_tag(:li) {
+              concat link_to 'Take', story_path(story, 'story[user_id]' => current_user), method: :put
+            }
+            concat content_tag(:li) {
+              concat link_to 'Take & start', story_path(story, 'story[state]' => 'started', 'story[user_id]' => current_user), method: :put
+            }
+          end
+          concat content_tag(:li, '', class: 'divider')
+          concat content_tag(:li) {
+            concat link_to 'Reject', story_path(story, 'story[state]' => 'rejected'), method: :put
+          }
         end)
       end
     else
       story.state
     end
 
-
-    #content_tag(:div, :class => "list") do
-    #  collection.collect do |member|
-    #    content_tag(:li, :id => member.name.gsub(' ', '-').downcase.strip) do
-    #      member.name
-    #    end
-    #  end
-    #end
-
-    #button
   end
 end
